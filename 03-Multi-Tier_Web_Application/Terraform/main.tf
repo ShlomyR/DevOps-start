d3c85ce0a512cc78823b8effd3d2184f0231e230
ShlomyR <rshlomy@gmail.com>
2025-04-01 17:24:43 +0300

    changing "allocation_method" to Static

--- 03-Multi-Tier_Web_Application/Terraform/main.tf
+++ 03-Multi-Tier_Web_Application/Terraform/main.tf
@@ -50,7 +50,8 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "devops-public-ip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
+  sku                = "Standard" # Ensure Standard SKU is explicitly set
 }
 
 resource "azurerm_network_interface" "nic" {
